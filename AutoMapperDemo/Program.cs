3b994540f0f0e2fe74702a0f991a81cd4cd4167e
张儒 <dazhangru@hotmail.com>
2019-07-17 17:18:17 +0800

    automapper例子

---
+++ AutoMapperDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDto>());
+            var mapper = config.CreateMapper();
+            Order order = new Order { OrderId = 1000, Price = "100.00", ProductId=1 };
+            OrderDto dto = mapper.Map<OrderDto>(order);
+            Console.WriteLine("");
+        }
+    }
+    class Order
+    {
+        public int OrderId { get; set; }
+        public string Price { get; set; }
+        public int ProductId { get; set; }
+    }
+    class OrderDto
+    {
+        public int OrderId { get; set; }
+        public string Price { get; set; }
+        public string ProductName { get; set; }
+    }
+}
